0d3299db7888f4aad713c2cfac50a32b05130bf5
chonlon <lonelycooler.xcl@gmail.com>
2020-01-14 16:02:16 +0800

    add:日志记录

---
+++ Qt/简单日志记录.cpp
@@ -0,0 +1,29 @@
+#include <QFile>
+#include <QTextStream>
+#include <QTextCodec>
+#include <QDateTime>
+const char* g_path = "./dalsa_debug_information.log";
+
+void writeToFile(const QString& str)
+{
+    QFile log_file{g_path};
+    if (!log_file.exists())
+        qDebug() << "No Such File" << __FILE__ << __FUNCTION__;
+    if (!log_file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
+        qWarning() << "File Open Failed" << __FILE__ << __FUNCTION__;
+    }
+
+    QTextStream stream{&log_file};
+    QTextCodec* codec = QTextCodec::codecForName("UTF-8");
+    stream.setCodec(codec);
+
+    QString log;
+    log.append('[').append(QDateTime::currentDateTime().toString("yy-M-d h:m:s")).append("] ");
+    log.append(str);
+    log.append('\n');
+    stream << log;
+
+    stream.flush();
+
+    log_file.close();
+}
