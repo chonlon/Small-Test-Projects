6ea96807c71bb6691a0828b1291c3251baee16c5
chonlon <lonelycooler.xcl@gmail.com>
2020-01-15 09:30:44 +0800

    fix: folly future 使用

--- C++/folly/future1.cpp
+++ C++/folly/future1.cpp
@@ -1,26 +1,34 @@
-#include <folly/futures/Future.h>
+// 在官方示例中稍作修改的小栗子
+
 #include <folly/executors/ThreadedExecutor.h>
+#include <folly/futures/Future.h>
 #include <iostream>
 
 using namespace std;
 using namespace folly;
 
-void foo(int x)
-{
+int foo(int x) {
     cout << "foo(" << x << ")" << endl;
+    return x + 1;
 }
 
-int main()
-{
+int main() {
     ThreadedExecutor executor;
     cout << "making Promise" << endl;
     Promise<int> p;
-    Future<int> f = p.getSemiFuture().via(&executor);
+    Future<int>  f =
+        p.getSemiFuture().via(&executor);
     auto f2 = move(f).thenValue(foo);
     cout << "Future chain made" << endl;
 
     cout << "fulfilling Promise" << endl;
     p.setValue(42);
-    move(f2).get();
+    // move(f2).get();
+
+    auto f3 = move(f2).thenValue(
+        [](int a) -> int { return a * a; });
+
+    cout << "value:" << move(f3).get() << endl;
+
     cout << "Promise fulfilled" << endl;
 }
