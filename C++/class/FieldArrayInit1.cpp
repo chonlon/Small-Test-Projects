774fcc885376643a4d10fe4969a2752a25898d74
chonlon <lonelycooler.xcl@gmail.com>
2020-01-15 09:30:28 +0800

    add: 数组初始化

---
+++ C++/class/FieldArrayInit1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class myprogram {
+public:
+    myprogram();
+
+private:
+    double aa, bb, cc;
+    double G_[2000];
+};
+myprogram::myprogram()
+    : aa{0.0}, bb{0.0}, cc{0.0}, G_{} {}
+
+int main() {
+    int a;
+    std::cout << a;
+    int arr[10];
+    myprogram p;
+}
