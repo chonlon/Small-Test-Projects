6ff766d5b61bbed8acb21184b3d805b4d080b708
chonlon <lonelycooler.xcl@gmail.com>
2020-01-30 17:30:30 +0800

    feat: add  init filed test  to see which init action is higher level

---
+++ C++/class/filedinit1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "../header/base.h"
+
+// 测试class里面成员变量初始化采用的方式
+
+
+
+class Object{
+    int var = 10;
+
+    IntField int_field_ = 10;
+public:
+    Object() = default;
+    // 可以看出来这里的优先级是高于上面的 var =10 的优先级的
+    // 并且并没有执行两次初始化IntField.
+    Object(int _var) : var(_var), int_field_(_var) {}
+
+    void print() {
+        std::cout << "var value: " << var << std::endl;
+        int_field_.print();
+    }
+};
+
+int main() {
+    printDividing();
+    Object{}.print();
+    printDividing();
+    Object{5}.print();
+}
