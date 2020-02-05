d046cf4b9ea846563ac7313d33fe549c538d6bd6
chonlon <lonelycooler.xcl@gmail.com>
2020-02-05 09:04:22 +0800

    add: basic pthread use.

---
+++ C++/thread/pthread1.cpp
@@ -0,0 +1,28 @@
+// pthread的基本使用
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int x = 0;
+
+void *mythread(void *arg) {
+    x = 1;
+    printf("Child process set x=1\n");
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t tid;
+    void *vp;
+    if(pthread_create(&tid, NULL, mythread, NULL) != 0) {
+        perror("thread_create");
+        exit(-1);
+    }
+
+    if(pthread_join(tid) != 0) {
+        perror("pthread_join");
+        exit(-1);
+    }
+    printf("parent process sees x = %d\n", x);
+    return 0;
+}
