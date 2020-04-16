876e03801d33f99237184a7fcbbd0e3cdf3cffca
chonlon <lonelycooler.xcl@gmail.com>
2020-04-16 13:54:29 +0800

    benifit: 对于decltype以及declval的概念的测试更加明晰

--- C++/basic/decltype1.cpp
+++ C++/basic/decltype1.cpp
@@ -1,22 +1,30 @@
-#include <utility>
+#include "../header/base.h"
 #include <iostream>
+#include <utility>
 
-struct Default
-{
-    int foo() const { return 1; }
+struct Default {
+    int foo() const {
+        PRINT_INVOKED();
+        return 1;
+    }
 };
 
-struct NonDefault
-{
-    NonDefault(const NonDefault &) {}
-    int foo() const { return 1; }
+struct NonDefault {
+    NonDefault() = delete;// make it clearer that defalut consturctor was deleted.
+    NonDefault(const NonDefault&) {}
+    int foo() const {
+        PRINT_INVOKED();
+        return 1;
+    }
 };
 
-int main()
-{
-    decltype(Default().foo()) n1 = 1;                   // type of n1 is int
-    //decltype(NonDefault().foo()) n2 = n1;               // error: no default constructor
-    decltype(std::declval<NonDefault>().foo()) n2 = n1; // type of n2 is int
+int main() {
+    decltype(Default().foo()) n1 = 1;  // type of n1 is int
+    std::cout << typeid(n1).name() << "\n";
+    printDividing();
+    decltype(std::declval<NonDefault>().foo()) n2 = n1;  // type of n2 is int
+    std::cout << typeid(n2).name() << "\n";
+    printDividing();
     std::cout << "n1 = " << n1 << '\n'
               << "n2 = " << n2 << '\n';
 }
