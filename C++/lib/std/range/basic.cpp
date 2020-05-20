50674e023b3a758f4fd2a5c2a194b0ed94771bf1
chonlon <lonelycooler.xcl@gmail.com>
2020-05-20 15:19:00 +0800

    add: range basic usage

---
+++ C++/lib/std/range/basic.cpp
@@ -0,0 +1,23 @@
+// current compile command: g++10 -std=c++20 1.cpp
+
+#include <iostream>
+#include <ranges>
+#include <vector>
+
+int main() {
+    std::vector<int> ints{0, 1, 2, 3, 4, 5};
+    auto even = [](int i) { return 1 == i % 2; };
+    auto square = [](int i) { return i * i; };
+
+    for (int i : ints | std::views::filter(even) |
+             std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+
+    std::cout << "\n------------\n";
+
+    for (long long i : std::views::iota(1, 100) |
+             std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+}
