494725ef2309210b4ec73a59e0098dc541953e55
chonlon <lonelycoler.xcl@gmail.com>
2020-09-07 21:29:08 +0800

    fix: fbvector add one custom alloctor which inherited from std::alloctor, and debug to see how fbvector works.

--- C++/folly/fbvector.cpp
+++ C++/folly/fbvector.cpp
@@ -3,6 +3,24 @@
 #include <folly/FBString.h>
 #include <iostream>
 
+template <typename T>
+class myalloctor : public std::allocator<T>
+{
+private:
+    using Allocator = std::allocator<T>;
+    int unused;
+public:
+    myalloctor() : Allocator() {}
+    /* implicit */ myalloctor(const Allocator& alloc)
+        : Allocator(alloc) {}
+    /* implicit */ myalloctor(Allocator&& alloc)
+        : Allocator(std::move(alloc)) {}
+    myalloctor(size_t n, const Allocator& alloc)
+    : Allocator(alloc) {}
+};
+
+// not using std alloctor will not use memcpy to move and will destory old memory manually.
+
 int main() {
     folly::fbvector<int> v{1, 2, 3};
     
@@ -32,7 +50,15 @@ int main() {
         }
     }
     std::cout << std::endl;
-    std::cout << std::endl;
 
+
+    std::cout << "<===================================>\n";
+    folly::fbvector<int, myalloctor<int>> mfv{1, 2, 3};
+    for(auto i = 4; i < 10; ++i) {
+        mfv.push_back(1);
+    }
+    for(auto& i : mfv) {
+        std::cout << i << ' ';
+    }
     return 0;
 }
