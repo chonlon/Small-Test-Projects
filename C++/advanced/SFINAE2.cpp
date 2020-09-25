9ae0896b9e70e757207505f42d839e7277c54c08
chonlon <lonelycoler.xcl@gmail.com>
2020-09-25 21:59:44 +0800

    add: sfinae example.

---
+++ C++/advanced/SFINAE2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <type_traits>
+// check if a class has func.
+// spefic name 'func'
+// to have universal usage: you need macro.
+
+
+struct A{
+    void func(){}
+};
+
+struct B {
+
+};
+
+struct C
+{
+    void func(int) {}
+};
+
+struct class_has_func {
+    template<typename T, typename... Args>
+    static constexpr bool test(decltype(std::declval<T>().func(std::declval<Args...>()))*) {
+        return true;
+    }
+
+    template<typename T>
+    static constexpr bool test(decltype(std::declval<T>().func())*) {
+        return true;
+    }
+
+    template<typename, typename>
+    static constexpr bool test(...) {
+        return false;
+    }
+};
+
+template <typename T, typename... Args>
+using has_func = typename std::conditional_t<class_has_func::template test<T, Args...>(nullptr), std::true_type, std::false_type>;
+
+
+
+int main() {
+    std::cout << has_func<A>::value << ' ' << has_func<B, int>::value  << ' ' << has_func<C, int>::value  << '\n';
+}
