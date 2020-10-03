c0e2ff3fe627b53c905d8d885b75915ff30c95b9
chonlon <lonelycoler.xcl@gmail.com>
2020-10-03 20:10:44 +0800

    fix: uncomment

--- scripts/shell/init_computer/install_vcpkg.sh
+++ scripts/shell/init_computer/install_vcpkg.sh
@@ -1,6 +1,6 @@
 #vcpkg
-#cd ~ && mkdir 2_lib && cd 2_lib
-#git clone https://github.com/microsoft/vcpkg.git 
+cd ~ && mkdir 2_lib && cd 2_lib
+git clone https://github.com/microsoft/vcpkg.git 
 cd ~/2_lib/vcpkg && sh ./bootstrap-vcpkg.sh
 ./vcpkg install boost:x64-linux
 ./vcpkg install folly:x64-linux
