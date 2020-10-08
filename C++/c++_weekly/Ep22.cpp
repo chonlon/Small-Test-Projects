dc7d5542356be15b9d701bca298714c0b7156d68
chonlon <lonelycoler.xcl@gmail.com>
2020-10-08 10:01:20 +0000

    add: (c++ weekly) Ep22

---
+++ C++/c++_weekly/Ep22.cpp
@@ -0,0 +1,21 @@
+#include "../header/base.h"
+
+#include <algorithm>
+
+namespace org::group::project::internal {
+    struct MyStruct {};
+}
+
+int main(int argc, char** argv) {
+    {
+        // nested namespace build success.
+        org::group::project::internal::MyStruct s;
+    }
+    
+    {
+        CaseMarker c{ "clamp" };
+        std::cout << (argc < 2 ? 2 : ((argc > 5) ? 5 : argc)) << '\n';
+        // same with boost clamp.
+        std::cout << std::clamp(argc, 2, 5) << '\n';
+    }
+}
