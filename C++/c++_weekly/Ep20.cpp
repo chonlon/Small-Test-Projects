0e3478a62cd457e862c6baf08817eac823616124
chonlon <lonelycoler.xcl@gmail.com>
2020-10-08 10:01:20 +0000

    add: (c++ weekly) Ep20

---
+++ C++/c++_weekly/Ep20.cpp
@@ -0,0 +1,45 @@
+#include "../header/base.h"
+#include <iostream>
+//see https://zh.cppreference.com/w/cpp/language/fold
+
+template <typename ... T>
+auto sum(T ... t) {
+    return (t + ...);
+}
+
+template <typename ... T>
+auto avg(T ... t) {
+    return (t + ...) / sizeof...(t);
+}
+
+template <typename ... T>
+auto div(T ... t) {
+    const int i = 5;
+    return ( i / ... / t );
+}
+
+template <typename ... T>
+auto div2(T ... t) {
+    const int i = 5;
+    return (... / t) / i;
+}
+
+template <typename ... T>
+void print(T ... t) {
+    (std::cout << ... << t) << '\n';
+}
+
+int main() {
+    {
+        CaseMarker c{ "cals" };
+        std::cout << sum(1, 1.2, 3, 4) << '\n';
+        std::cout << avg(1.0, 2.0, 3.0, 4.0) << '\n';
+        std::cout << div(1.0, 2.0, 3.0) << '\n';// rst = 5/1/2/3
+        std::cout << div2(1.0, 2.0, 3.0) << '\n';// rst = 1/2/3/5.
+    }
+
+    {
+        CaseMarker c{ "print" };
+        print(1, 2, 3, "123");
+    }
+}
