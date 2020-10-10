840144aa7e62cfd5672eda5b2e715133e85ccff4
chonlon <lonelycoler.xcl@gmail.com>
2020-10-10 23:14:11 +0800

    fix: (scripts/sh) 在2_lib存在的情况下git clone到正确位置上

--- scripts/shell/init_computer/install_vcpkg.sh
+++ scripts/shell/init_computer/install_vcpkg.sh
@@ -1,7 +1,9 @@
 #vcpkg
-cd ~ && mkdir 2_lib && cd 2_lib
+mkdir ~/2_lib
+cd ~/2_lib
 git clone https://github.com/microsoft/vcpkg.git 
 cd ~/2_lib/vcpkg && sh ./bootstrap-vcpkg.sh
-./vcpkg install boost:x64-linux
 ./vcpkg install folly:x64-linux
+./vcpkg install boost:x64-linux
 ./vcpkg install range-v3:x64-linux
+./vcpkg integrate install
