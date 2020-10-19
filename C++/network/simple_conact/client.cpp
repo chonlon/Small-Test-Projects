725902792713fe7691d219efdfc6ae2c28154618
chonlon <lonelycoler.xcl@gmail.com>
2020-10-19 12:46:06 +0800

    fixed: (c++ network) 1. contact client 使用asio由于connect完成响应函数处于loop forever中, 所以不会进"事件循环", 后面的响应函数永远不会执行 2. 对于没有直接apt安装boost开发包的系统, 还是需要find boost的, 对于已经安装了得, 由于cmake再/cmake_prefix_dir/shared/下面提供了查找脚本, 所以也能正常工作.

--- C++/network/simple_conact/client.cpp
+++ C++/network/simple_conact/client.cpp
@@ -31,31 +31,59 @@ void parseCommandLine(int argc, char** argv, Address& address) {
 void connectAndSendUtilStop(const Address& address) {
     using namespace boost::asio;
     using boost::asio::ip::tcp;
-    try{
+    try {
         io_service service;
         //ip::tcp::acceptor acceptor(service, tcp)
         tcp::endpoint endpoint(ip::address::from_string(address.host_name), address.port);
         std::shared_ptr<tcp::socket> socket = std::shared_ptr<tcp::socket>(new tcp::socket(service));
         //socket.connect(endpoint);
         socket->async_connect(endpoint,
-            [address, socket](const boost::system::error_code& error) {
+            [address, socket, &service](const boost::system::error_code& error) mutable {
                 if (error) { return; }
-                std::cout << "connect to server " << address.host_name << ":" << address.port << "successed!\n";
+                std::cout << "connect to server " << address.host_name << " : " << address.port << " successed!\n";
                 //socket->async_send
-                for (;;) {
-                    socket->async_write_some(buffer("some text\n"),
-                        [](const boost::system::error_code& error, size_t len) {
-                            if (!error) {
-                                std::cout << "send completed.\n";
-                            }
-                        }
-                    );
-                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                constexpr int write_buf_size = 100;
+                char write_buf[write_buf_size]{ 'l', 'o', 'v', 'e' };
+                for (int i = 4; i < write_buf_size; ++i) {
+                    write_buf[i] = '+';
+                }
+                char read_buf[100]{ 0 };
+                int i = 0;
+                socket->async_read_some(buffer(read_buf, 100),
+                    [&](const boost::system::error_code& error, std::size_t len) mutable {
+                        // if (!error) {
+                        
+                        if (*read_buf == EOF) {
+                            service.stop();
+                        };
+                        std::cout << read_buf;
+                        // } else {
+                        //     std::cout << error.message();
+                        //     return;
+                        // }
+                    }
+                );
+                for (;i < 12;) {
+                    ++i;
+                    std::cout << "mark zero\n";
+                    boost::asio::async_write(*socket, buffer(write_buf,write_buf_size),
+                        [socket, read_buf](const boost::system::error_code& error, std::size_t len) mutable {
+                            printf("send completed");
+                            std::cout << "send completed.\n";
+                        });
+                    // socket->async_write_some(buffer(write_buf),
+                    // );
+                    std::cout << "mark one " << i << '\n';
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
                 }
             }
         );
         service.run();
-    } catch (std::exception& e) {
+        std::cout << "end\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+    }
+    catch (std::exception& e) {
         std::cout << e.what();
     }
 }
