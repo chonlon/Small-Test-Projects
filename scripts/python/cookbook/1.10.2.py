ab00dc2def14f5e44cdb7cb8c725545ac8cdfdd4
chonlon <lonelycoler.xcl@gmail.com>
2020-10-19 12:34:18 +0800

    add: (python cookbook) 1.10.2.

---
+++ scripts/python/cookbook/1.10.2.py
@@ -0,0 +1,11 @@
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+dedupe(a)
+print(a)
+
+print(list(dedupe(a)))
