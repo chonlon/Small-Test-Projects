7f3c50175d31b61adcaa16c5a8544aff7e596a85
chonlon <lonelycoler.xcl@gmail.com>
2020-10-27 18:04:33 +0800

    add: (python cookbook) 4.6.

---
+++ scripts/python/cookbook/4.6.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class linehistory:
+    def __init__(self, lines, histlen = 3):
+        self.lines = lines
+        self.history = deque(maxlen = histlen)
+
+    def __iter__(self):
+        for lineno, line in enumerate(self.lines, 1):
+            self.history.append((lineno, line))
+            yield line
+
+    def clear(self):
+        self.history.clear()
+
+
+with open('./files/1.txt') as f:
+    lines = linehistory(f)
+    for line in lines:
+        if 'python' in line:
+            for lineno, hline in lines.history:
+                print('{}:{}'.format(lineno, hline), end='')
