e3829460d7cc43add79714aba72963cb75f91151
chonlon <lonelycoler.xcl@gmail.com>
2020-10-27 18:04:34 +0800

    add: (python cookbook) 6.1.

---
+++ scripts/python/cookbook/6.1.py
@@ -0,0 +1,16 @@
+import csv
+
+headers = ['Symbol', 'Price', 'Date', 'Time', 'Change', 'Volume']
+rows = [('AA', 39.48, '6/11/2007', '9:36am', -0.18, 181800),
+        ('AIG', 71.38, '6/11/2007', '9:36am', -0.15, 195500),
+        ('AXP', 62.58, '6/11/2007', '9:36am', -0.46, 935000),
+        ]
+
+with open('files/stocks.csv', 'w') as f:
+    f_csv = csv.writer(f)
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
+
+
+
+
