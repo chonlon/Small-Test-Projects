e87f22de06726835757048f35391365ab82d48ba
chonlon <lonelycoler.xcl@gmail.com>
2020-12-19 19:11:47 +0800

    modified: (C++) small vector debug test

--- C++/lib/folly/vector/small_vector.cpp
+++ C++/lib/folly/vector/small_vector.cpp
@@ -2,12 +2,18 @@
 #include <base.h>
 
 int main() {
+    [[maybe_unused]] folly::small_vector<int, 3> _vec{{1, 2, 3}};
+    
+
     constexpr int size = 4;
-    folly::small_vector<int, size> vec{{1,2,3,4}};
+
+    folly::small_vector<int, size> vec{{1,2,3}};
+    
     std::cout << std::hex;
     printDividing("on stack");
     std::cout << reinterpret_cast<long>(&vec[0]) << '\n';
+    vec.push_back(4);
     printDividing("on heap");
-    vec.push_back(1);
+    vec.push_back(5);
     std::cout << reinterpret_cast<long>(&vec[0]) << '\n';
 }
