924d2b4fed8c60c83c44e6d3b63956218bd8b9d4
chonlon <lonelycoler.xcl@gmail.com>
2021-03-10 19:08:16 +0800

    fix: port from args

--- scripts/python/network/TCP/EchoServer.py
+++ scripts/python/network/TCP/EchoServer.py
@@ -1,6 +1,6 @@
 import socket
 import selectors
-import time
+import argparse
 
 
 def recv_data(conn):
@@ -19,13 +19,16 @@ def acc_conn(p_server):
 
 
 if __name__ == "__main__":
-    CONN_ADDR = ("127.0.0.1", 2006)
+    parser = argparse.ArgumentParser(description='tcp server options')
+    parser.add_argument('-p', '--port', default='22222',help='listen to port', dest='port')
+    args = parser.parse_args()
+    CONN_ADDR = ("127.0.0.1", int(args.port))
     server = socket.socket()
     server.bind(CONN_ADDR)
     server.listen(6)
     e_poll = selectors.DefaultSelector()
     e_poll.register(server, selectors.EVENT_READ, acc_conn)
-    print("port: 2006 wating for connect...")
+    print("port: {}  wating for connect...".format(int(args.port)))
     while True:
         # 事件循环不断地调用select获取被激活的socket
         events = e_poll.select()
