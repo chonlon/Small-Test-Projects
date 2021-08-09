eea5d78c92119c31df4bd97eb5cb0a862d56df75
chonlon <lonelycooler.xcl@gmail.com>
2021-08-09 16:44:46 +0800

    add: occt demo

---
+++ C++/compter_graphic/occt_demo/StepReader.hpp
@@ -0,0 +1,116 @@
+//
+// Created by lon on 2021/8/9.
+//
+
+#ifndef MINE_OCCT_DEMO_STEPREADER_HPP
+#define MINE_OCCT_DEMO_STEPREADER_HPP
+
+auto dumpLabelName (const TDF_Label& label, std::ostream& os,std::string attribute) -> std::ostream& {
+  Handle(TDataStd_Name)  labelName;
+  label.FindAttribute(TDataStd_Name::GetID(), labelName);
+  os << attribute;
+  labelName->Dump(os);
+  os << '\n';
+};
+
+class StepReader {
+public:
+    StepReader(std::string_view file_name) : file_name_(file_name) {
+
+    }
+
+    std::ostream &print(std::ostream& os) const {
+      auto root_label = doc_handle_->Main();
+      auto shape_tool = XCAFDoc_DocumentTool::ShapeTool(root_label);
+      auto color_tool = XCAFDoc_DocumentTool::ColorTool(root_label);
+
+
+
+      os << "\n\n===========dumping object: " << file_name_ << "==================\n";
+      if(print_base_)  {
+        auto base_label = shape_tool->BaseLabel();
+
+        os << "\n\n-------------dumping base label---------------\n";
+        {// base label name
+          dumpLabelName(base_label, os,"base label name : ");
+        }
+
+        {// base label children
+          int count = 0;
+          for(TDF_ChildIterator it{base_label}; it.More(); it.Next()) {
+            ++count;
+            TDF_Label child_label = it.Value();
+            TopoDS_Shape sub_shape = shape_tool->GetShape(child_label);
+
+            if(!sub_shape.IsNull()) {
+              os << "\nsub shape:\n\t";
+              dumpLabelName(child_label, os,"sub label name: ");
+              os << "\tshape json:\n\t\t";
+              sub_shape.DumpJson(os);
+              os << '\n';
+            }
+          }
+          os << "\t---count: " << count << "---\n";
+        }
+
+        os << "\n\n--------------dumping free labels------------\n";
+        {
+          TDF_LabelSequence free_labels;
+          shape_tool->GetFreeShapes(free_labels);
+          int count = 0;
+          for(TDF_LabelSequence::Iterator it(free_labels); it.More(); it.Next()) {
+            os << ++count << "---\n";
+            TDF_Label child_free_label = it.Value();
+            TopoDS_Shape sub_shape = shape_tool->GetShape(child_free_label);
+            if(!sub_shape.IsNull())
+              continue;
+            dfsTrSubLabel(child_free_label, os, shape_tool);
+          }
+        }
+      }
+
+      return os;
+    }
+
+    void dfsTrSubLabel(const TDF_Label& label, std::ostream& os, Handle(XCAFDoc_ShapeTool)& shape_tool ) const {
+      dumpLabelName(label, os,"free label:");
+      if(label.HasChild()) {
+        dfsTrSubLabel(label, os, shape_tool);
+      } else {
+        TopoDS_Shape sub_shape = shape_tool->GetShape(label);
+        sub_shape.DumpJson(os);
+      }
+    }
+    bool parse() {
+      STEPCAFControl_Reader reader;
+
+      reader.SetColorMode(Standard_True);
+      reader.SetNameMode(Standard_True);
+      reader.SetLayerMode(Standard_True);
+
+      IFSelect_ReturnStatus status = reader.ReadFile(file_name_.data());
+      if(status != IFSelect_RetDone) {
+        std::cout << "read file failed! which: " << file_name_;
+        return false;
+      }
+
+      Handle(TDocStd_Application) app = new TDocStd_Application();
+      app->NewDocument("MDTV-XCAF", doc_handle_);// 这里的format是什么//在TDocStd_Document构造函数中没有说明
+      reader.Transfer(doc_handle_);
+      return true;
+    }
+private:
+
+
+    bool print_color_ = true;
+    //    bool print_shape_ = true;
+    bool print_base_ = true;
+    bool print_freedom_ = true;
+    std::string_view file_name_;
+    Handle(TDocStd_Document) doc_handle_;
+};
+std::ostream &operator<<(std::ostream& os, StepReader& reader){
+  return reader.print(os);
+}
+
+#endif //MINE_OCCT_DEMO_STEPREADER_HPP
---
+++ C++/compter_graphic/occt_demo/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <STEPCAFControl_Reader.hxx>
+#include <TDocStd_Application.hxx>
+#include <TDocStd_Document.hxx>
+#include <argparse.hpp>
+#include <regex>
+#include <XCAFDoc_DocumentTool.hxx>
+#include <XCAFDoc_ShapeTool.hxx>
+#include <XCAFDoc_Color.hxx>
+#include <TDataStd_Name.hxx>
+#include <exception>
+#include <TDF_ChildIterator.hxx>
+#include "StepReader.hpp"
+
+
+
+int main(int argc, char** argv) {
+    argparse::ArgumentParser program("demo", "0.0.1");
+    program.add_argument("-i").required().help("specify the input step type module file");
+    program.add_argument("-o")
+    .help("result output file");
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+
+    std::string input_file_name = program.get<std::string>("-i");
+    std::string output_file_name;
+    try {
+      output_file_name = program.get<std::string>("-o");
+    } catch (...) {
+    }
+
+    std::ostream* out = nullptr;
+
+
+    if(output_file_name.empty()) {
+      out = &std::cout;
+    } else {
+      out = new std::ofstream (output_file_name, std::ios::app | std::ios::binary);
+    }
+
+    std::regex regStp(".+\\.ste?pz?$", std::regex_constants::icase);
+    if(!std::regex_match(input_file_name, regStp)) {
+        std::cout << "Invalid step file ext.";
+    }
+    StepReader stepReader(input_file_name);
+    if(!stepReader.parse()) {
+        return -1;
+    }
+
+
+    *out << stepReader;
+    if(!output_file_name.empty()) {
+      delete out;
+    }
+
+    return 0;
+}
