c730dd3d0168ff2912610b7381e5273af017b531
chonlon <lonelycooler.xcl@gmail.com>
2021-08-30 16:52:58 +0800

    perf(python): clash srcipt

--- scripts/python/StartupClash.py
+++ scripts/python/StartupClash.py
@@ -8,14 +8,15 @@ clash_config_file = '{}/config.yaml'.format(clash_dir)
 if os.path.isfile(clash_subscribe_link_file):
     with open(clash_subscribe_link_file) as f:
         url = f.readline()
-        print('Downloading {}'.format(url))
+        url = url.rstrip()
+        print('Downloading {} ...'.format(url))
         print(clash_subscribe_link_file)
         print(clash_config_file)
         # wget.download(url, clash_dir)
-        os.system('wget --no-check-certificate {} -O {}'.format(url, clash_config_file))
+        os.system('wget --no-check-certificate --no-proxy {} -O {}'.format(url, clash_config_file))
 else:
-    print('File not Exists!')
+    print('File not Exists! Please add clashlink to clash config directory which file name is {}'.format(clash_subscribe_link_file))
 
 
-os.system('{0}/clash-linux-amd64 -d {0}'.format(clash_dir))
+#os.system('{0}/clash-linux-amd64 -d {0}'.format(clash_dir))
 
