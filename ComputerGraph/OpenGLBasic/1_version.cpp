80bfab580916296de87c60747a05a315c9040150
chonlon <lonylycooler.xcl@gmail.com>
2021-12-06 14:56:46 +0800

    :sparkles: 初始化glfw窗口.

--- ComputerGraph/OpenGLBasic/1_version.cpp
+++ ComputerGraph/OpenGLBasic/1_version.cpp
@@ -3,9 +3,47 @@
 //
 
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 
 int main() {
-  std::cout << "hello world!";
+  // init glfw window ...
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // create glfw window ...
+  auto window = glfwCreateWindow(900, 600, "1_version", nullptr, nullptr);
+  if(window == nullptr) {
+    std::cout << "Failed to create glfw windows\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  // init glad context
+  if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+    std::cout << "Failed to initialize GLAD\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  // equal frame buffer size?
+  glViewport(0, 0, 800, 600);
+
+  // set resize call back function
+  auto framebuffer_resize_cb = [](GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+  };
+  glfwSetFramebufferSizeCallback(window, framebuffer_resize_cb);
+
+  while(!glfwWindowShouldClose(window)) {
+    // 一般都需要swap buffer, 那么是程序框架做好了framebuffer, 来保证双缓冲吗?
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  return 0;
 }
