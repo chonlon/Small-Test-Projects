bd37b12fedeab06c14c6b50947078947ab706f71
chonlon <lonylycooler.xcl@gmail.com>
2022-04-29 19:25:41 +0800

    add rga support

--- scripts/python/cli-tools/fzf-open.py
+++ scripts/python/cli-tools/fzf-open.py
@@ -7,6 +7,7 @@ import sys
 
 ls_app = 'logo-ls'
 cat_app = 'bat'
+rg_app = 'rga'
 
 # define editor apps will be used
 editor_apps = ['vim', 'code', 'ranger', 'kate', 'clion', 'pycharm']
@@ -17,7 +18,7 @@ dir_apps = ['cd', 'code', 'ranger', 'clion', 'pycharm', 'dolphin',]
 
 # check if the cli app is installed
 def check_clis_installed():
-    all_apps = [ls_app, cat_app, 'fzf', 'fd']
+    all_apps = [ls_app, cat_app, 'fzf', 'fd', rg_app]
     for app in editor_apps:
         all_apps.append(app)
     for app in dir_apps:
@@ -43,7 +44,7 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
 
     # get path from command line, default is current directory, not required
-    argparser.add_argument('path', default='.', nargs='?')
+    argparser.add_argument('path', default='.', nargs='+')
     # get if type is file or directory, default is file, not required, can be file or directory
     argparser.add_argument('-t', '--type', default='f', choices=['f', 'd'])
     # get pattern from command line
@@ -58,25 +59,42 @@ if __name__ == "__main__":
     argparser.add_argument("-x", "--exec", help="execute command",
                           default=False, action="store_true")
 
+    # get file filter by content
+    argparser.add_argument("-c", "--content", help="filter file by content provided")
+
     args = argparser.parse_args()
 
-    path = args.path
+    paths = args.path
     file_type = args.type
     pattern = args.pattern
     search_all = args.all
     open_cli = args.open
     if_exec = args.exec
+    content_filter = args.content
+    if not content_filter:
+        cat_app = cat_app
+    else:
+        cat_app = '{rg_app} --pretty --context 5 {content_filter} {{}}'\
+            .format(rg_app=rg_app, content_filter=content_filter)
+
+
 
     # fd in path and fzf select item and pipe to fzf
     command = 'fd --type {file_type} --exclude=.git {ignore} {pattern}  {path}'.\
         format(file_type=file_type,
                ignore='-H --ignore' if search_all else '',
                pattern=pattern,
-               path=path)
+               path= ' '.join(paths))
+    print(command)
+
+    if content_filter and file_type == 'f':
+        command = '{command} | xargs rga {content_filter} --files-with-matches '.\
+            format(command=command, content_filter=content_filter)
 
     command += '|'
-    command += 'fzf --preview "{} {}" --select-1'.format(
+    command += 'fzf --sort --preview "{} {}" --select-1'.format(
         cat_app if file_type == 'f' else ls_app, '{}')
+
     # get command output
     try:
         output = subprocess.check_output(command, shell=True)
