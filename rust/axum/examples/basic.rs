4d843d23115078149a2bb5c0331e1e98f28f5b5e
chonlon1 <lonelycooler.xcl@gmail.com>
2022-05-13 22:26:06 +0800

    add login and create todo api with post

--- rust/axum/examples/basic.rs
+++ rust/axum/examples/basic.rs
@@ -1,8 +1,13 @@
 use std::net::SocketAddr;
 use serde::{Deserialize, Serialize};
 
-use axum::{routing::get, Router, Server, Json};
+use axum::{routing::{get, post},Router, Server, Json};
+use axum::http::StatusCode;
 use axum::response::Html;
+
+use jsonwebtoken as jwt;
+
+const SECRET: &[u8] = b"secret";
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct Todo {
@@ -11,11 +16,29 @@ struct Todo {
     completed: bool,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct CreateTodo {
+    title: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct LoginRequest {
+    email: String,
+    password: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct LoginResponse {
+    token: String,
+}
+
 #[tokio::main]
 async fn main() {
     let router = Router::new()
         .route("/", get(index_handler))
-        .route("/todos", get(todohandler).post(create_todohandler));
+        .route("/todos", get(todo_handler).post(create_todo_handler))
+        .route("/login", post(login_handler));
+
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on {}", addr);
@@ -26,15 +49,35 @@ async fn index_handler() -> Html<String> {
     Html("<h1>Hello, world!</h1>".to_string())
 }
 
-async fn todohandler() -> Json<String> {
-    let todo = Todo {
-        id: 1,
-        title: "Learn Rust".to_string(),
-        completed: false,
-    };
-    Json::from(serde_json::to_string(&todo).unwrap())
+async fn todo_handler() -> Json<Vec<Todo>> {
+    Json(vec![
+        Todo {
+            id: 1,
+            title: "Todo 1".to_string(),
+            completed: false,
+        },
+        Todo {
+            id: 2,
+            title: "Todo 2".to_string(),
+            completed: false,
+        },
+    ])
 }
 
-async fn create_todohandler() {
-    todo!();
+async fn create_todo_handler(Json(create_todo): Json<CreateTodo>) -> StatusCode {
+    println!("accepted {:?}", create_todo);
+    StatusCode::CREATED
+}
+
+
+async fn login_handler(Json(login): Json<LoginRequest>) -> Json<LoginResponse> {
+    println!("accepted {:?}", login);
+
+    let key = jwt::EncodingKey::from_secret(SECRET);
+    let token = jwt::encode(&jwt::Header::default(), &login, &key).unwrap();
+    println!("token: {}", token);
+
+    Json(LoginResponse {
+        token: jwt::encode(&jwt::Header::default(), &login, &key).unwrap(),
+    })
 }
